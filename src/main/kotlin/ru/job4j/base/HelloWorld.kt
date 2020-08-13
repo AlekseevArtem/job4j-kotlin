e9137e9cc5585259e1e7ae7783315678f383e838
AlekseevArtem <tema9991@mail.ru>
2020-08-13 19:22:01 +0300

    2. Типы данных и методы. [#340079]

--- src/main/kotlin/ru/job4j/base/HelloWorld.kt
+++ src/main/kotlin/ru/job4j/base/HelloWorld.kt
@@ -1,5 +1,32 @@
 package ru.job4j.base
 
+fun add(first : Int, second : Int) : Int {
+    return first + second
+}
+
+fun subtract(first : Int, second : Int) : Int {
+    return first - second
+}
+
+fun div(first : Int, second : Int) : Int {
+    return first / second
+}
+
+fun multiply(first : Int, second : Int) : Int {
+    return first * second
+}
+
 fun main() {
-    println("Hello from Kotlin")
+    val plus = add(1, 1)
+    println("1 + 1 = $plus")
+
+    val minus = subtract(1, 1)
+    println("1 - 1 = $minus")
+
+    val division = div(4, 2)
+    println("4 / 2 = $division")
+
+    var multiply = multiply(2, 2)
+    multiply = multiply(multiply, 2)
+    println("2 * 2 * 2 = $multiply")
 }
