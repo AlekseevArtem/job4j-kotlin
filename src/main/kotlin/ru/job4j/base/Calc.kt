5a47001f226c66b6e42bab6dbd29948c85b39144
AlekseevArtem <tema9991@mail.ru>
2020-08-13 20:30:58 +0300

    4. Операторы условий if [#340081]

--- src/main/kotlin/ru/job4j/base/Calc.kt
+++ src/main/kotlin/ru/job4j/base/Calc.kt
@@ -16,6 +16,12 @@ fun multiply(first : Int, second : Int) : Int {
     return first * second
 }
 
+fun max(first: Int, second: Int) = if (first > second) first else second
+
+fun max(first: Int, second: Int, third: Int) = if (first > second) if (first > third) first else third else if (second > third) second else third
+
+//fun max(first: Int, second: Int, third: Int) = if (max(first,second) > third) max(first,second) else third
+
 fun main() {
     val plus = add(1, 1)
     println("1 + 1 = $plus")
@@ -29,4 +35,10 @@ fun main() {
     var multiply = multiply(2, 2)
     multiply = multiply(multiply, 2)
     println("2 * 2 * 2 = $multiply")
+
+    val rslForTwo = max(1, 2)
+    println("max from 1 and 2 is $rslForTwo")
+
+    val rslForThree = max(4, 2, 3)
+    println("max from 4, 2 and 3 is $rslForThree")
 }
--- src/test/kotlin/ru/job4j/base/CalcTest.kt
+++ src/test/kotlin/ru/job4j/base/CalcTest.kt
@@ -19,4 +19,12 @@ class CalcTest : StringSpec({
     "2 * 2 = 4" {
         multiply(2, 2) shouldBe 4
     }
+
+    "max from 1 and 2 is 2" {
+        max(1, 2) shouldBe 2
+    }
+
+    "max from 5 and 4 is 3" {
+        max(5, 4, 3) shouldBe 5
+    }
 })
