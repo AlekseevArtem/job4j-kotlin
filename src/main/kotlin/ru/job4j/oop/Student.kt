c26484b5732176b99d20cb3f6136beedb6727ad0
AlekseevArtem <tema9991@mail.ru>
2020-08-14 19:13:10 +0300

    2.1. Конструкторы. [#340087]

---
+++ src/main/kotlin/ru/job4j/oop/Student.kt
@@ -0,0 +1,24 @@
+package ru.job4j.oop
+
+class Student {
+    private var name: String = ""
+    private var surname: String = ""
+    private var phone: String = ""
+    private var email: String = ""
+
+    constructor(name: String) {
+        this.name = name
+    }
+
+    constructor(name: String, surname: String) : this(name) {
+        this.surname = surname
+    }
+
+    constructor(name: String, surname: String, phone: String) : this(name,surname) {
+        this.phone = phone
+    }
+
+    constructor(name: String, surname: String, phone: String, email: String) : this(name, surname, phone) {
+        this.email = email
+    }
+}
