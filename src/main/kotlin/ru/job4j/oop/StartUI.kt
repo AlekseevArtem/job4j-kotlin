bb3e2b22b9df727980ae5aaf68fce0b0dd256956
AlekseevArtem <tema9991@mail.ru>
2020-08-14 20:00:28 +0300

    4. Класс-объект. [#340089]

---
+++ src/main/kotlin/ru/job4j/oop/StartUI.kt
@@ -0,0 +1,23 @@
+package ru.job4j.oop
+
+object StartUI {
+    internal fun init(tracker: Tracker, select: Int) {
+        var run = true
+        while (run) {
+            when (select) {
+                0 -> {
+                    println(tracker.add(Item("name")))
+                }
+                1 -> {
+                    val all: List<Item> = tracker.findAll()
+                    all.forEach{
+                        println(it)
+                    }
+                }
+                2 -> {
+                    run = false
+                }
+            }
+        }
+    }
+}
