b59eb4f61d77810fbf9baf0731a059fefb4f785c
AlekseevArtem <tema9991@mail.ru>
2020-08-16 22:23:25 +0300

    6. With [#340101]

---
+++ src/main/kotlin/ru/job4j/lambda/EmailService.kt
@@ -0,0 +1,14 @@
+package ru.job4j.lambda
+
+class EmailService {
+
+    fun emailTo(message: Message): String {
+        return with(StringBuilder()) {
+            append("Subject : ").append(message.email)
+            append("Body : ").append("Hello, ").append(message.email).append(" ")
+            append("You win!")
+        }.toString()
+    }
+}
+
+data class Message(val username: String, val email: String)
