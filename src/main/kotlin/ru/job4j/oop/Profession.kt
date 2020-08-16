49f32d6d853cb02488ef4538875c0407d5577ffd
AlekseevArtem <tema9991@mail.ru>
2020-08-16 13:49:17 +0300

    8. Переопределение. [#340093]

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -1,15 +1,50 @@
 package ru.job4j.oop
 
-open class Profession(name: String, surname: String) {
+open class Profession(private val name: String, private val surname: String) {
     private var birthday: String = ""
 
     constructor(name: String, surname: String, birthday: String) : this(name, surname) {
         this.birthday = birthday
     }
+
+    open fun action() {
+        println("execute some actions")
+    }
+
+    open fun getName(): String {
+        return name
+    }
 }
 
-class Doctor(name: String, surname: String, birthday: String, specialty: String) : Profession(name, surname, birthday)
+class Doctor(name: String, surname: String, birthday: String, specialty: String) : Profession(name, surname, birthday) {
+    override fun action() {
+        super.action()
+        println("action from doctor")
+    }
 
-class Developer(name: String, surname: String, level: String) : Profession(name, surname, birthday = "11.11.1999")
+    override fun getName(): String {
+        return "doctor " + super.getName()
+    }
+}
 
-class Singer(name: String, surname: String, voiceType: String) : Profession(name, surname)
+class Developer(name: String, surname: String, level: String) : Profession(name, surname, birthday = "11.11.1999") {
+    override fun action() {
+        super.action()
+        println("action from developer")
+    }
+
+    override fun getName(): String {
+        return "developer " + super.getName()
+    }
+}
+
+class Singer(name: String, surname: String, voiceType: String) : Profession(name, surname) {
+    override fun action() {
+        super.action()
+        println("action from singer")
+    }
+
+    override fun getName(): String {
+        return "singer " + super.getName()
+    }
+}
