7e6e8f15b2c1eeab701630246765d5ec54606020
AlekseevArtem <tema9991@mail.ru>
2020-08-17 12:39:12 +0300

    Упростил with по рекомендации Петра

--- src/main/kotlin/ru/job4j/lambda/EmailService.kt
+++ src/main/kotlin/ru/job4j/lambda/EmailService.kt
@@ -2,13 +2,11 @@ package ru.job4j.lambda
 
 class EmailService {
 
-    fun emailTo(message: Message): String {
-        return with(StringBuilder()) {
-            append("Subject : ").append(message.email)
-            append("Body : ").append("Hello, ").append(message.email).append(" ")
-            append("You win!")
-        }.toString()
-    }
+    fun emailTo(message: Message): String = with(StringBuilder()) {
+        append("Subject : ").append(message.email)
+        append("Body : ").append("Hello, ").append(message.email).append(" ")
+        append("You win!")
+    }.toString()
 }
 
 data class Message(val username: String, val email: String)
