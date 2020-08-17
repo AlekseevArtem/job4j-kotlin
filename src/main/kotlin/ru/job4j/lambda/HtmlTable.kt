95b00333d3791aa1b0a97db7bc1513be589db8a2
AlekseevArtem <tema9991@mail.ru>
2020-08-17 12:42:26 +0300

    Упростил apply по рекомендации Петра

--- src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
+++ src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
@@ -1,18 +1,14 @@
 package ru.job4j.lambda
 
 class HtmlTable {
-    fun table(row: Int, cell: Int): String {
-        val table = StringBuilder()
-        table.apply {
-            append("<table>")
-            append("<caption>Table</caption>")
-            append("<tr>")
-            for(index in 0..row){
-                append("<th>$index row</th>")
-            }
-            append("</tr>")
-            append("</table>")
+    fun table(row: Int, cell: Int): String = StringBuilder().apply {
+        append("<table>")
+        append("<caption>Table</caption>")
+        append("<tr>")
+        for (index in 0..row) {
+            append("<th>$index row</th>")
         }
-        return table.toString()
-    }
+        append("</tr>")
+        append("</table>")
+    }.toString()
 }
